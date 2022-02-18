0386c47f0b518f2d210ae337f72bb7e00d620737
harsharora99 <hdsingharora99@gmail.com>
2022-02-18 12:09:05 +0530

    Initial Commit

---
+++ movie-mania/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  movie-mania
+//
+//  Created by harshdeep.s on 18/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
